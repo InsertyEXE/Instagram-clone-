3c92cdc95d79fb2d9f0bdddfb3bec1da67166a2e
InsertyEXE <insertyend@gmail.com>
2022-04-28 15:03:40 -0300

    feat: mais codigo na fragmentProfile
    a fragment continuou sua criação, ainda não está finalizada

--- app/src/main/java/com/insertu/instagramclonekotlin/comum/model/UserAuth.kt
+++ app/src/main/java/com/insertu/instagramclonekotlin/comum/model/UserAuth.kt
@@ -4,5 +4,10 @@ data class UserAuth(
     val uuid: String,
     val name: String,
     val email: String,
-    val password: String
-)
+    val password: String,
+    val countPost: Int,
+    val countFollowing: Int,
+    val countFollowers: Int
+) {
+
+}
--- app/src/main/java/com/insertu/instagramclonekotlin/profile/view/PostAdapter.kt
+++ app/src/main/java/com/insertu/instagramclonekotlin/profile/view/PostAdapter.kt
@@ -1,13 +1,17 @@
 package com.insertu.instagramclonekotlin.profile.view
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.insertu.instagramclonekotlin.R
+import com.insertu.instagramclonekotlin.comum.model.Post
 
 class PostAdapter : RecyclerView.Adapter<PostAdapter.postViewHolder>() {
+
+    var itens: List<Post> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): postViewHolder {
         return postViewHolder(
@@ -17,7 +21,7 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.postViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: postViewHolder, position: Int) {
-        holder.bind(R.drawable.ic_insta_add)
+        holder.bind(itens[position].uri)
     }
 
     override fun getItemCount(): Int {
@@ -25,9 +29,9 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.postViewHolder>() {
     }
 
     class postViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(imagem: Int) {
+        fun bind(imagem: Uri) {
             itemView.findViewById<ImageView>(R.id.item_profile_img_grid)
-                .setImageResource(imagem)
+                .setImageURI(imagem)
         }
     }
 
